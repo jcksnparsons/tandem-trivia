13d0c0807793f15fc9efcff91435b59be6ceba58
Jack Parsons <jcksnparsons@yahoo.com>
2020-10-28 15:27:25 -0500

    refactored to accomodate for questions that don't have 4 answer choices. would like to refactor to allow letters for answers instead of numbers again

--- main.py
+++ main.py
@@ -22,7 +22,8 @@ for i in data[:10]:
 
 def prompt_for_answer():
     global score
-    user_answer = input("Please select an answer... ")
+    user_answer = int(input("Please select an answer... "))
+    print("")
 
     if user_answer in answer_key_list:
         if answers_dict[user_answer] == question['correct']:
@@ -33,18 +34,22 @@ def prompt_for_answer():
             print(f"So close! The answer was {question['correct']}")
 
         print(f"Your score is now {score}/10")
+        print("")
 
     else:
         print("Oops, not a valid answer! Try again ")
-        prompt_for_answer(score)
+        prompt_for_answer()
 
-input("Welcome to Tandem for 400! Press enter to continue... ")
+input("Welcome to Tandem for 400! Press any key to continue... ")
+print("")
 
 for question in questions:
     print(question['question'])
     random.shuffle(question['answers'])
 
-    answer_key_list = ['A','B','C','D']
+    answer_key_list = []
+    for i in range(len(question['answers'])):
+        answer_key_list.append(i)
 
     answers_dict = dict(zip(answer_key_list, question['answers']))
 
