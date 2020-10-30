6cd6c5e1b10451db55ac6fc86bcdd0e6a6eb4284
Jack Parsons <jcksnparsons@yahoo.com>
2020-10-30 09:13:12 -0500

    refactored so question array contains entire class instance instead of dict representation, changed code to cal params on that object, renamed i variable for clarity's sake

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ class trivia_question:
 
 random.shuffle(data)
 
-for i in data[:10]:
-    question = trivia_question(i['question'], i['incorrect'], i['correct'])
-    questions.append(question.__dict__)
+for q_object in data[:10]:
+    question = trivia_question(q_object['question'], q_object['incorrect'], q_object['correct'])
+    questions.append(question)
 
 def prompt_for_answer():
     global score
@@ -28,12 +28,12 @@ def prompt_for_answer():
     print("")
 
     try:
-        if answers_dict[int(user_answer)] == question['correct']:
+        if answers_dict[int(user_answer)] == question.correct:
             print("Correct!")
             score += 1
             
         else:
-            print(f"So close! The answer was {question['correct']}")
+            print(f"So close! The answer was {question.correct}")
 
         print(f"Your score is now {score}/10")
         print("")
@@ -46,14 +46,14 @@ input("Welcome to Tandem for 400! Press any key to continue... ")
 print("")
 
 for question in questions:
-    print(question['question'])
-    random.shuffle(question['answers'])
+    print(question.question)
+    random.shuffle(question.answers)
 
     answer_key_list = []
-    for i in range(len(question['answers'])):
+    for i in range(len(question.answers)):
         answer_key_list.append(i)
 
-    answers_dict = dict(zip(answer_key_list, question['answers']))
+    answers_dict = dict(zip(answer_key_list, question.answers))
 
     for key, value in answers_dict.items():
         print(f'{key}: {value}')
